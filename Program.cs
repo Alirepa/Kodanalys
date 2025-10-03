1f93f55c6cc5deaff585fd76b2aa9810f567ad0d
Ali <panahi.ali0606@gmail.com>
2025-10-03 17:23:05 +0300

    Separerade ansvarsområden:
    Flyttade menyvisning till DisplayMenu()
    
    Flyttade valhantering till ProcessUserChoice()
    
    Använder switch för renare kontrollflöde
    
    Förbättrad kodstruktur:
    
    Main-metoden är nu minimalistisk och lättläst
    
    Varje metod har ett tydligt, enskilt syfte

--- Program.cs
+++ Program.cs
@@ -2,47 +2,54 @@
 {
     class Program
     {
-        static List<string> users = new List<string>();
+        private static List<string> users = new List<string>();
 
         static void Main(string[] args)
         {
             bool isRunning = true;
+
             while (isRunning)
             {
-                Console.WriteLine("Välj ett alternativ:");
-                Console.WriteLine("1. Lägg till användare");
-                Console.WriteLine("2. Visa alla användare");
-                Console.WriteLine("3. Ta bort användare");
-                Console.WriteLine("4. Sök användare");
-                Console.WriteLine("5. Avsluta");
+                DisplayMenu();
                 string userChoice = Console.ReadLine();
-
-                if (userChoice == "1")
-                {
-                    AddUser();
-                }
-                else if (userChoice == "2")
-                {
-                    ShowAllUsers();
-                }
-                else if (userChoice == "3")
-                {
-                    RemoveUser();
-                }
-                else if (userChoice == "4")
-                {
-                    SearchUser();
-                }
-                else if (userChoice == "5")
-                {
-                    isRunning = false;
-                }
-                else
-                {
-                    Console.WriteLine("Ogiltigt val.");
-                }
-                Console.WriteLine();
+                isRunning = ProcessUserChoice(userChoice);
             }
+        }
+
+        static void DisplayMenu()
+        {
+            Console.WriteLine("Välj ett alternativ:");
+            Console.WriteLine("1. Lägg till användare");
+            Console.WriteLine("2. Visa alla användare");
+            Console.WriteLine("3. Ta bort användare");
+            Console.WriteLine("4. Sök användare");
+            Console.WriteLine("5. Avsluta");
+        }
+        static bool ProcessUserChoice(string choice)
+        {
+            switch (choice)
+            {
+                case "1":
+                    AddUser();
+                    break;
+                case "2":
+                    ShowAllUsers();
+                    break;
+                case "3":
+                    RemoveUser();
+                    break;
+                case "4":
+                    SearchUser();
+                    break;
+                case "5":
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Console.WriteLine("Ogiltigt val.");
+                    break;
+            }
+            Console.WriteLine();
+            return true;
         }
 
         static void AddUser()
@@ -56,7 +63,7 @@
         static void ShowAllUsers()
         {
             Console.WriteLine("Användare:");
-            foreach (string user in users) 
+            foreach (string user in users)
             {
                 Console.WriteLine(user);
             }
@@ -66,9 +73,9 @@
         {
             Console.Write("Ange namn att ta bort: ");
             string userName = Console.ReadLine();
-            
-            if (users.Contains(userName)) 
-            { 
+
+            if (users.Contains(userName))
+            {
                 users.Remove(userName);
                 Console.WriteLine("Användaren borttagen.");
             }
@@ -82,10 +89,10 @@
         {
             Console.Write("Ange namn att söka: ");
             string userName = Console.ReadLine();
-            
-            if (users.Contains(userName)) 
+
+            if (users.Contains(userName))
             {
-            Console.WriteLine("Användaren hittades.");
+                Console.WriteLine("Användaren hittades.");
             }
             else
             {
